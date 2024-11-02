cc9fa12cc8599f8825ba3585d1476b62afff9a31
Featherwalk <vacantorange@gmail.com>
2024-11-02 14:30:14 -0500

    added initial styles

---
+++ src/styles/themes.js
@@ -0,0 +1,19 @@
+// Default theme colors
+const JET_BLACK = "#111111";
+const BRIGHT_YELLOW = "#CCFF00";
+const MIDNIGHT = "#002642";
+const SHADOW = "#333333";
+const GHOST = "#f2f2f2";
+
+// add more themes here eventually maybe
+const DEFAULT = {
+  background: SHADOW,
+  border: JET_BLACK,
+  callout: BRIGHT_YELLOW,
+  text: GHOST,
+  accent: MIDNIGHT,
+};
+
+export default {
+  DEFAULT,
+};
